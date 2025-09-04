7ec17608e0a83089ea9ff8aa164dc67b57c08b24
Mateus Cardozo <mateusczdev@gmail.com>
2025-09-04 09:42:29 -0300

    Adicionado package-lock.json

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -5,7 +5,7 @@ export default function initAnimacaoScroll() {
   function animaScroll() {
     sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - windowMetade) < 0;
+      const isSectionVisible = sectionTop - windowMetade < 0;
       if (isSectionVisible) {
         section.classList.add('ativo');
       } else if (section.classList.contains('ativo')) {
