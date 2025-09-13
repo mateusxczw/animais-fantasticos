9728e6c0c65268a98510770886b3ab0bd1371451
Mateus Cardozo <mateusczdev@gmail.com>
2025-09-12 21:28:58 -0300

    Adicionado slide de fotos

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import DropdownMenu from './modules/dropdown-menu.js';
 import MenuMobile from './modules/menu-mobile.js';
+import SlideNav from './modules/slide.js';
 import Funcionamento from './modules/funcionamento.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
@@ -46,3 +47,8 @@ funcionamento.init();
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+
+slide.addControl('.custom-control');
